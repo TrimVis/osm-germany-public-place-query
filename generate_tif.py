8ba5596b67bf83972e88fda9893a28e1324dc8fd
Pasquale Jordan <jordanpa@ethz.ch>
2024-04-11 21:30:16 +0200

    Added comment

--- generate_tif.py
+++ generate_tif.py
@@ -287,6 +287,9 @@ if __name__ == "__main__":
             # tif might not be the best format for this tbh 😅
             # I will have to look into some possible alternatives that are
             # vector based (don't know of any so far)
+            # NOTE pjordan: Alternatively if tif is the only option
+            # we might need to split germany into sectors or go over things 
+            # on a per state basis, or similiar
             create_german_raster(resolution=0.0001,
                                  max_workers=MAX_WORKERS,
                                  no_smoke_wkt=no_smoke_pedestrian_wkt,
